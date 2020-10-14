aa9bb13988b4906ad07060792a610f1dbeea9525
Victor Pereira <victor.cordeiro2@gmail.com>
2020-10-14 12:44:37 -0300

    fix: orphanage_view faltando nome

--- backend/src/views/orphanages_view.ts
+++ backend/src/views/orphanages_view.ts
@@ -6,6 +6,7 @@ export default {
     render(orphanage: Orphanage) {
         return {
             id: orphanage.id,
+            name: orphanage.name,
             latitude: orphanage.latitude,
             longitude: orphanage.longitude,
             about: orphanage.about,
